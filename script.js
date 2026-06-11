25dac7c1bc04c184b8548958ef1fcdfb4d613954
harikrishnay2020-alt <harikrishnay2020@gmail.com>
2026-06-11 16:05:54 +0530

    java-programming

---
+++ script.js
@@ -0,0 +1,31 @@
+let name ="Hari";
+var age = 20;
+const pi = 3.14;
+console.log(name);
+console.log(age+5);
+console.log(age+9);
+console.log(age+10);
+console.log(age+11);
+age+=5;
+console.log(age);
+age-=5;
+age1=2;
+console.log(age1>age);
+let food = "biryani";
+let breakfast = "idly";
+if(food=="breakfast")
+    {
+        console.log("eat idly for breakfast");
+    }
+    else if(food=="lunch")
+    {
+        console.log("eat biryani for lunch");
+    }
+    for(let i=1;i<=30;i++)
+    {
+        console.log("the day is "+i+" completed");
+    }
+    for(let i=1;i<=10;i++)
+    {
+        console.log("the table of 2 is "+2*i);
+    }
